41cbd0631875d45064681585833efe200dcb6108
Rayna Weeden <imagineskies@protonmail.com>
2020-06-06 18:14:10 -0400

    Update

--- git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git init
-git add --all
-git commit -m "first commit"
-git remote add origin https://github.com/Tinker-Labs-L3C/StaticSite.git
-git push -u origin master
-echo "Done!"
-exit
